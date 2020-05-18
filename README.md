125f70abfdefd131ed9e262af6b1c53dfb77f7c1
Alistair Dutton <alistair.dutton@ovoenergy.com>
2020-05-18 21:48:57 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+ep
+==
+
+Simple date converter. Prints out the "date thing" converted to:
+
+* An instant
+* Epoch days
+* Epoch millis
+
+Usage
+-----
+
+* From source:
+
+```
+clj -Arun <date-thing>
+```
+
+Or from an uberjar
+
+* Build from source: `clj -Auberjar`
+* Run `java -jar target/ep-1.0.0-standalone.jar <date-thing>`
---
+++ src/ep/core.clj
@@ -0,0 +1,63 @@
+(ns ep.core
+  (:gen-class)
+  (:require [java-time :as jt]
+            [puget.printer :as puget])
+  (:import (java.time ZoneOffset)))
+
+(def ^:private millis-per-day (* 60 60 24 1000))
+
+(defn to-instant
+  "Attempts to convert the given x to a date/time. If only a date is specified then 00:00 on that date is
+  implied as the time."
+  [x]
+  (cond
+    (jt/instant? x) x
+
+    (nil? x) nil
+
+    (jt/local-date? x)
+    (-> (.atStartOfDay x)
+        (jt/instant ZoneOffset/UTC))
+
+    (string? x)
+    (try
+      (-> x jt/zoned-date-time jt/instant)
+      (catch Exception e
+        (if (clojure.string/ends-with? x "T00:00Z")
+          (throw e)
+          (to-instant (str x "T00:00Z")))))
+
+    :else
+    (if (> x 30000)
+      (jt/instant x)
+      (jt/instant (* x millis-per-day)))))
+
+(defn to-date-time-string
+  [x]
+  (some-> x to-instant str))
+
+(defn to-epoch-millis
+  "Attempts to convert the given x to a date/time and coerce to millis since epoch. If only a date is specified then
+  00:00 on that date is implied as the time."
+  [x]
+  (some-> x to-instant jt/to-millis-from-epoch))
+
+(defn to-epoch-days
+  "Attempts to convert the given x to a date/time and coerce to days since epoch. If only a date is specified then
+  00:00 on that date is implied as the time."
+  [x]
+  (some-> x to-epoch-millis (quot millis-per-day)))
+
+(defn ep
+  "Attempts to convert the given x to a date/time and returns a summary. If only a date is specified then
+  00:00 on that date is implied as the time."
+  [x]
+  {:date-time    (to-date-time-string x)
+   :epoch-millis (to-epoch-millis x)
+   :epoch-days   (to-epoch-days x)})
+
+
+(defn -main [& [x]]
+  (let [date-thing (try (Long/parseLong x)
+                        (catch Exception _ x))]
+    (puget/cprint (ep date-thing))))
